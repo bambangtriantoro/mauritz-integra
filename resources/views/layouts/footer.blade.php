82f3b325479c889ce4ca4c8d0897569f850a603e
rizky <rizkyaditya2106@gmail.com>
2022-07-27 14:56:19 +0700

    adjusting

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -17,7 +17,7 @@
                     to
                     you!</p>
             </div>
-            <div class="col-12 col-sm-12 col-md-3 col-lg-2">
+            <div class="col-12 col-sm-12 col-md-3 col-lg-2 ms-lg-5 ps-lg-5">
                 <h5>Tautan</h5>
                 <ul>
                     <li><a href="/home">Beranda</a></li>
