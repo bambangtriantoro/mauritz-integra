1dd4cfa94c6762dfa4d4fd708625a1bfbaaf0f2f
rizky <rizkyaditya2106@gmail.com>
2022-09-26 10:08:16 +0700

    adjusting

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,38 +8,25 @@
 
     <div class="container">
         <section class="about-us-desc desc1">
-            <h2 class="text-center mb-5">Tentang Perusahaan <span>dan</span> Founder</h2>
+            <h2 class="text-center mb-5">Tentang <span>Maurindo</span> Group</h2>
             <div class="row justify-content-center align-items-start">
                 <p class="col-12 col-md-7">
                     <b>
-                        Mauceng Martinus Simanjuntak (MAURINDO GROUP) adalah seorang profesional bidang Penjualan dan
-                        Pemasaran yang telah meraup segudang pengalaman dalam bidang Farmasi dan Obat Obatan, Telekomunikasi
-                        dan IT serta di bidang peralatan Teknologi Digital selama lebih dari 20 tahun. Sebelum menjabat
-                        sebagai Founder dan CEO PT. MAURINDO GROUP INDONESIA, dengan anak perusahaan PT. MAURINDO TEKNIK
-                        SOLUSI dan PT MAURINDO ALKES INDONESIA, beliau sudah mempunya relasi yang kuat di bidang Farmasi,
-                        Telekomunikasi dan IT pada perusahaan Nasional dan Multinasional terkemuka di Indonesia. Dengan
-                        bekal tersebut lah, beliau mendirikan MAURINDO GROUP pada tahun 2015 sebagai landasan usaha.
-                        <br> <br>
-                        Pria yang lahir di kota Laguboti, Kota kecil di kabupaten Sumatera Utara ini, berjuang dan
-                        memberikan kontribusi terbaik baik negara dengan dedikasi dan pengalaman yang sudah di dapatkan
-                        selama 20 tahun lebih berkarya.
-                        <br> <br>
-                        Dengan Spesialisasi yang beliau miliki dibidang bisnis dan services, terampil dipenjualan, dinamis,
-                        serta telah menghasilkan karya-karya inovatif serta berorientasi pada hasil yang secara konsisten
-                        melampaui tujuan bisnis dengan memanfaatkan keterampilan membangun tim untuk kepuasan pelanggan
-                        adalah dasar di dirikannya MAURINDO GROUP.
+                        MAURINDO GROUP yang berdiri pada tahun 2015 dengan anak perusahaan PT. MAURINDO TEKNIK SOLUSI dan PT
+                        MAURINDO ALKES INDONESIA, dengan spesialisasi yang dimiliki dibidang bisnis dan services, terampil
+                        dipenjualan, dinamis, serta telah menghasilkan karya-karya inovatif serta berorientasi pada hasil
+                        yang secara konsisten melampaui tujuan bisnis dengan memanfaatkan keterampilan membangun tim untuk
+                        kepuasan pelanggan adalah dasar di dirikannya MAURINDO GROUP.
                         <br> <br>
                         Masalah dan komplen yang muncul adalah senjata dan acuan kami untuk lebih kuat dan lebih baik.
                         komunikasi yang kuat dengan semua pihak dari berbagai kalangan akan memberikan solusi pada masalah
                         yang di hadapai serta kemampuan untuk berkreasi dan memimpin untuk bisa menjadi acuan dalam
-                        berkompetisi menjalankan usaha alat kesehatan. Beliau sebagai Individu yang termotivasi dengan latar
-                        belakang yang kuat dalam manajemen penjualan dan menjalin hubungan relasi yang panjang dengan
-                        berbagai pihak pendukung dan dengan tekat serta pengalaman maka terbangun lah MAURINDO GROUP.
+                        berkompetisi menjalankan usaha alat kesehatan.
                         <br> <br>
-                        Dengan lebih dari 30 orang tenaga professional (Sales Marketing Dept, Operation dept, Keuangan dan
-                        Finance Dept dan Purchasing Dept) dan dengan pengalaman lebih dari 6 tahun, MAURINDO GROUP
-                        memberikan solusi dan produk-produk terbaik bagi pelanggan. Dengan penghasilan lebih dari 60 Milliar
-                        rupiah pertahun mengukuhkan diri sebagai salah satu perusahaan penyedia Alat Kesehatan terbaik.
+                        Dengan lebih dari 30 orang tenaga professional (Sales Marketing Dept, Operation dept, Finance Dept
+                        dan Purchasing Dept) dan dengan pengalaman lebih dari 6 tahun, MAURINDO GROUP memberikan solusi dan
+                        produk-produk terbaik bagi pelanggan. Dengan penghasilan lebih dari 60 Milliar rupiah pertahun
+                        mengukuhkan diri sebagai salah satu perusahaan penyedia Alat Kesehatan terbaik.
                     </b>
                 </p>
                 <img src="asset/image/logo/logo2-1.jpeg" alt="" class="col-12 col-md-5">
